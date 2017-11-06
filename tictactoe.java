034be8cbc582ab3b75011a00e501a23de6dbdab3
Joel Khoo <joelkhoo@gmail.com>
2017-11-06 15:12:26 +0800

    Rewrote hasWon function to check n size boards

--- tictactoe.java
+++ tictactoe.java
@@ -17,12 +17,12 @@ public class tictactoe {
    public static final int NOUGHT_WON = 3;
  
    // The game board and the game status
-   public static final int ROWS = 3, COLS = 3; // number of rows and columns
-   public static int[][] board = new int[ROWS][COLS]; // game board in 2D array
+   public static int ROWS = 3, COLS = 3; // number of rows and columns
+   public static int[][] board; // game board in 2D array
                                                       //  containing (EMPTY, CROSS, NOUGHT)
    public static int currentState;  // the current state of the game
                                     // (PLAYING, DRAW, CROSS_WON, NOUGHT_WON)
-   public static int availableCellsLeft = ROWS * COLS;
+   public static int availableCellsLeft;
    public static int currentPlayer; // the current player (CROSS or NOUGHT)
    public static int currntRow, currentCol; // current seed's row and column
  
@@ -31,7 +31,10 @@ public class tictactoe {
    /** The entry main method (the program starts here) */
    public static void main(String[] args) {
       // Initialize the game-board and current status
-      initGame();
+      System.out.print("How big a board do you wish to play?: ");
+      int size = in.nextInt();
+
+      initGame(size);
       // Play the game once
       do {
          playerMove(currentPlayer); // update currentRow and currentCol
@@ -51,9 +54,13 @@ public class tictactoe {
    }
  
    /** Initialize the game-board contents and the current states */
-   public static void initGame() {
-      for (int row = 0; row < ROWS; ++row) {
-         for (int col = 0; col < COLS; ++col) {
+   public static void initGame(int size) {
+      ROWS = COLS = size;
+      board = new int[ROWS][COLS];
+      availableCellsLeft = ROWS * COLS;
+
+      for (int row = 0; row < size; ++row) {
+         for (int col = 0; col < size; ++col) {
             board[row][col] = EMPTY;  // all cells empty
          }
       }
@@ -68,7 +75,7 @@ public class tictactoe {
       do {
 
          String playerName = (theSeed == CROSS)? "X" : "O";
-         System.out.print("Player '"+playerName+"', enter your move (row[1-3] column[1-3]): ");
+         System.out.print("Player '"+playerName+"', enter your move (row[1-"+ROWS+"] column[1-"+COLS+"]): ");
 
          int row = in.nextInt() - 1;  // array index starts at 0 instead of 1
          int col = in.nextInt() - 1;
@@ -97,18 +104,6 @@ public class tictactoe {
  
    /** Return true if it is a draw (no more empty cell) */
    // TODO: Shall declare draw if no player can "possibly" win
-   /*
-   public static boolean isDraw() {
-      for (int row = 0; row < ROWS; ++row) {
-         for (int col = 0; col < COLS; ++col) {
-            if (board[row][col] == EMPTY) {
-               return false;  // an empty cell found, not draw, exit
-            }
-         }
-      }
-      return true;  // no empty cell, it's a draw
-   }
-  */
 
    //optimization from O(N^2) to O(1) runtime
    public static boolean isDraw() {
@@ -123,42 +118,56 @@ public class tictactoe {
 
    public static boolean hasWon(int theSeed, int currentRow, int currentCol) {
 
+        //check col
+        for(int i = 0; i < ROWS; i++){
+            if(board[currentRow][i] != theSeed)
+                break;
+            if(i == ROWS-1){
+              return true;
+            }
+        }
 
-      return (board[currentRow][0] == theSeed         // 3-in-the-row
-                   && board[currentRow][1] == theSeed
-                   && board[currentRow][2] == theSeed
-              || board[0][currentCol] == theSeed      // 3-in-the-column
-                   && board[1][currentCol] == theSeed
-                   && board[2][currentCol] == theSeed
-              || currentRow == currentCol            // 3-in-the-diagonal
-                   && board[0][0] == theSeed
-                   && board[1][1] == theSeed
-                   && board[2][2] == theSeed
-              || currentRow + currentCol == 2  // 3-in-the-opposite-diagonal
-                   && board[0][2] == theSeed
-                   && board[1][1] == theSeed
-                   && board[2][0] == theSeed);
+        //check row
+        for(int i = 0; i < ROWS; i++){
+            if(board[i][currentCol] != theSeed)
+                break;
+            if(i == ROWS-1){
+              return true;
+            }
+        }
+
+        //check diagonal
+        if(currentRow == currentCol){
+            for(int i = 0; i < ROWS; i++){
+                if(board[i][i] != theSeed)
+                    break;
+                if(i == ROWS-1){
+                  return true;
+                }
+            }
+        }
+
+        //check opposite diagonal
+        if((currentRow + currentCol) == (ROWS - 1)){
+            for(int i = 0;i<ROWS;i++){
+                if(board[i][(ROWS-1)-i] != theSeed)
+                    break;
+                if(i == ROWS-1){
+                  return true;
+                }
+            }
+        }
+
+        return false;
    }
-/*       
-   public static boolean hasWon(int theSeed, int currentRow, int currentCol) {
-      return (board[currentRow][0] == theSeed         // 3-in-the-row
-                   && board[currentRow][1] == theSeed
-                   && board[currentRow][2] == theSeed
-              || board[0][currentCol] == theSeed      // 3-in-the-column
-                   && board[1][currentCol] == theSeed
-                   && board[2][currentCol] == theSeed
-              || currentRow == currentCol            // 3-in-the-diagonal
-                   && board[0][0] == theSeed
-                   && board[1][1] == theSeed
-                   && board[2][2] == theSeed
-              || currentRow + currentCol == 2  // 3-in-the-opposite-diagonal
-                   && board[0][2] == theSeed
-                   && board[1][1] == theSeed
-                   && board[2][0] == theSeed);
-   }
- */
+
    /** Print the game board */
    public static void printBoard() {
+      String x_partition = "";
+      for(int length =0 ; length<ROWS;++length){
+          x_partition+="----";
+      }
+
       for (int row = 0; row < ROWS; ++row) {
          for (int col = 0; col < COLS; ++col) {
             printCell(board[row][col]); // print each of the cells
@@ -167,8 +176,9 @@ public class tictactoe {
             }
          }
          System.out.println();
+
          if (row != ROWS - 1) {
-            System.out.println("-----------"); // print horizontal partition
+            System.out.println(x_partition); // print horizontal partition
          }
       }
       System.out.println();
